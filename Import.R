274ec7773889f515d8d92eb5878dc8833093ce3d
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-11-10 09:57:32 +0100

    Save in object

--- Import.R
+++ Import.R
@@ -1,4 +1,4 @@
 ## List of files in folder
 dir()
-## Import file
-read.table("M_R_White Nile State.csv", sep = ",")
+## Import file and save into rawdata object
+rawdata <- read.table("M_R_White Nile State.csv", sep = ",")
