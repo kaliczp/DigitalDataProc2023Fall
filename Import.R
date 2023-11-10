a6b7e3732cb4fe5d2339ebf1910121183d126767
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-11-10 10:18:51 +0100

    With header and summary

--- Import.R
+++ Import.R
@@ -1,4 +1,6 @@
 ## List of files in folder
 dir()
 ## Import file and save into rawdata object
-rawdata <- read.table("M_R_White Nile State.csv", sep = ",")
+rawdata <- read.table("M_R_White Nile State.csv", sep = ","header = TRUE)
+## Make a summary
+summary(rawdata)
