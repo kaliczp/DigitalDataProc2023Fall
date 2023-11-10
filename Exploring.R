8dbecc49ff48a371d1eadd7720661d95aa45df70
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-11-10 10:53:00 +0100

    Vector incexin

--- Exploring.R
+++ Exploring.R
@@ -1,2 +1,18 @@
 ## Yearly sums
 rowSums(rawdata)
+### Indexing
+## First row and firs column
+rawdata[1,1]
+## All Years
+rawYears <- rawdata[,1]
+## All data in first row
+rawdata[1,]
+### Vector indexing
+## Only one element
+rawYears[1]
+## Range with :, seq() function and manual vector creaation
+rawYears[1:10]
+rawYears[seq(1,10,1)]
+rawYears[c(1,2,3,4,5,6,7,8,9,10)]
+## Exclude the first element
+rawYears[-1]
