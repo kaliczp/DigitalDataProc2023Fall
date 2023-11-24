4fc8584430fccfeb92dfffef39de76fe7ef6c1f8
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-11-24 10:00:15 +0100

    Transform matrix

--- SimpleTS.R
+++ SimpleTS.R
@@ -1,2 +1,6 @@
+## Generate simple time-series from yearly sums
 YearlyPrec <- ts(data = rowSums(rawdata[,-1]), 1980, 2022)
+## Plot it
 plot(YearlyPrec, ylim = c(0, 450), yaxs = "i")
+## Access all montly data
+as.numeric(t(as.matrix(rawdata[,-1])))
