b743e0f46acd0d4d1c6e165f7e945212f1de3796
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-11-24 10:26:08 +0100

    Enhanced plot

--- SimpleTS.R
+++ SimpleTS.R
@@ -4,4 +4,7 @@ YearlyPrec <- ts(data = rowSums(rawdata[,-1]), 1980, 2022)
 plot(YearlyPrec, ylim = c(0, 450), yaxs = "i")
 ## Access all montly data
 MonthlyPrec <- ts(as.vector(t(as.matrix(rawdata[,-1]))), 1980, 2022, frequency = 12)
-plot(MonthlyPrec)
+plot(MonthlyPrec)ű
+## Modified plot
+?par
+plot(MonthlyPrec, ylim = c(0,210), xaxs = "i", lwd = 2, type = "h", yaxs = "i", ylab = "Monthly Precipitation [mm]")
