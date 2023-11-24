0d745643c0706a33bca912e9cab502721843b151
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-11-24 10:13:42 +0100

    Create monthly sercies of prec

--- SimpleTS.R
+++ SimpleTS.R
@@ -3,4 +3,5 @@ YearlyPrec <- ts(data = rowSums(rawdata[,-1]), 1980, 2022)
 ## Plot it
 plot(YearlyPrec, ylim = c(0, 450), yaxs = "i")
 ## Access all montly data
-as.numeric(t(as.matrix(rawdata[,-1])))
+MonthlyPrec <- ts(as.vector(t(as.matrix(rawdata[,-1]))), 1980, 2022, frequency = 12)
+plot(MonthlyPrec)
