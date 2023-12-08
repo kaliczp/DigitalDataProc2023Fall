153d976f16633b4aa2d35ae861b0448ad62e0473
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-12-08 10:46:41 +0100

    Plot our data sevaral ways

--- AnalysisTempR.R
+++ AnalysisTempR.R
@@ -7,6 +7,20 @@ PT.df <- data.frame(P = coredata(MonthlyPrec.xts['1981/2021']),
 plot(PT.df)
 ## Same as above with formula
 plot(T ~ P,data = PT.df)
+## Same as above with dollar variable selection
+plot(PT.df$P, PT.df$T)
+## With indexing
+plot(PT.df[, 1], PT.df[, 2])
+## with name index
+plot(PT.df[, "P"], PT.df[, "T"])
 ## Change aexes
 plot(P ~ T,data = PT.df)
+plot(PT.df$T, PT.df$P)
+plot(PT.df[, 2], PT.df[, 1])
+plot(PT.df[, "T"], PT.df[, "P"])
+
+## Correlation
+cor(PT.df)
+cor(PT.df, use = "complete.obs")
 ## Select rows where temperature available
+
