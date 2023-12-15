434a969c8736e6e1668d3e16a8870d4f200514c9
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2023-12-15 15:46:26 +0100

    Plot yearly tics for whole intervalum

--- PlotWithZoo.R
+++ PlotWithZoo.R
@@ -1,4 +1,4 @@
 plot.zoo(MonthlyTemp.xts)
 
 plot.zoo(MonthlyPrec.xts)
-axis.Date(1, at = as.Date(paste0("198",1:9,"-01-01")), labels = FALSE)
+axis.Date(1, at = as.Date(paste0(1981:2022,"-01-01")), labels = FALSE)
