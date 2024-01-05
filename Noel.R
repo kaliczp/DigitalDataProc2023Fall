3ee2bd455e7e416515f569ccc90b8c8bbb41eafd
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2024-01-05 15:14:51 +0100

    Training factor

--- Noel.R
+++ Noel.R
@@ -1,3 +1,8 @@
 install.packages("readxl")
 library(readxl)
 Sheema <- as.data.frame(read_excel("HH_Summary_Database_Sheema.xlsx"))
+summary(Sheema)
+plot(Sheema[,1:10])
+
+## Categorical variable
+Sheema$training <- factor(Sheema$training, levels = 0:1, labels = c("No", "Yes"))
