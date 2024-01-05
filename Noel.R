03b88fd106a98b87b2c6689972ef68a67633d7ae
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2024-01-05 15:49:11 +0100

    Some test and correlation

--- Noel.R
+++ Noel.R
@@ -6,3 +6,13 @@ plot(Sheema[,1:10])
 
 ## Categorical variable
 Sheema$training <- factor(Sheema$training, levels = 0:1, labels = c("No", "Yes"))
+
+## Correlation
+cor(Sheema$experience,Sheema$age)
+cor(Sheema$farm_size,Sheema$forest_area)
+plot(Sheema$farm_size,Sheema$forest_area)
+Sheema$farm_size > Sheema$forest_area
+plot(Sheema$farm_size,Sheema$coffee_area)
+abline(0, 1)
+Sheema$farm_size > Sheema$coffee_area
+Sheema[Sheema$farm_size < Sheema$coffee_area, c("farm_size", "coffee_area")]
