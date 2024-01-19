b2d32e331facd0bb6f3f331030643745cc06cae8
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2024-01-19 14:57:15 +0100

    Further correction of erroneous data

--- Osman.R
+++ Osman.R
@@ -4,3 +4,9 @@ OsmanRaw <- as.data.frame(read_excel("ABUBKR DATA.xlsx", sheet = "Sheet4", n_max
 which(is.na(as.numeric(OsmanRaw$CD_AR_SH)))
 OsmanRaw[76, "CD_AR_SH"] <- "3.9"
 OsmanRaw$CD_AR_SH <- as.numeric(OsmanRaw$CD_AR_SH)
+which(is.na(as.numeric(OsmanRaw$D_AT_T)))
+OsmanRaw[c(73, 350), "D_AT_T"]
+OsmanRaw[73, "DOM"] <- OsmanRaw[73, "D_AT_T"]
+OsmanRaw[73, "D_AT_T"] <- 0
+OsmanRaw[350, "D_AT_T"] <- 0
+OsmanRaw[350, "D_AT_T"] <- as.numeric(OsmanRaw[350, "D_AT_T"])
