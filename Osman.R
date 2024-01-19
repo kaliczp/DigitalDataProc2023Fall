32d9a8785ea8e2da6791c83b42062ad43fa64290
Kalicz Péter <kalicz.peter@uni-sopron.hu>
2024-01-19 14:47:41 +0100

    Correction of mistiped data

--- Osman.R
+++ Osman.R
@@ -1,2 +1,6 @@
 library(readxl)
 OsmanRaw <- as.data.frame(read_excel("ABUBKR DATA.xlsx", sheet = "Sheet4", n_max = 384))
+## Filter erroneous data
+which(is.na(as.numeric(OsmanRaw$CD_AR_SH)))
+OsmanRaw[76, "CD_AR_SH"] <- "3.9"
+OsmanRaw$CD_AR_SH <- as.numeric(OsmanRaw$CD_AR_SH)
